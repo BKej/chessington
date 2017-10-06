5f5a824e529be810b1ba2e993b7431101f6d8056
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-06 13:52:02 +0100

    Added code for rook.js file

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,4 +1,6 @@
 import Piece from './piece';
+import Player from '../player';
+import Square from '../square';
 
 export default class Rook extends Piece {
     constructor(player) {
@@ -6,6 +8,20 @@ export default class Rook extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+
+        let location = board.findPiece(this)
+        const moves = []
+
+        if (this.player === Player.WHITE) {
+            //Code to move Rook laterally i.e Horizontal or Vertical
+            for (let i = 0; i < 8; i++) {
+                if (i != location.col)
+                    moves.push(Square.at(location.row, i))
+                if (i != location.row)
+                    moves.push(Square.at(i, location.col))
+            }
+            return moves
+        }
     }
 }
+git
