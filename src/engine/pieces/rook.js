be28a5a8b266b7fca70c556954c2439e5e7383e2
Bindiya <bindiya.kejdiwal@gmail.com>
2023-01-24 11:51:16 +0000

    Added code for King and Black pieces, if they can be killed by white pieces

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Square from '../square';
+import King from './king';
 
 export default class Rook extends Piece {
     constructor(player) {
@@ -17,22 +18,46 @@ export default class Rook extends Piece {
             for (let i = location.col - 1; i >= 0; i--) {
                 if (board.getPiece(Square.at(location.row, i)) === undefined) {
                     moves.push(Square.at(location.row, i))
-                } else break
+                } else if ((board.getPiece(Square.at(location.row, i))).player === Player.BLACK
+                    && !((board.getPiece(Square.at(location.row, i))) instanceof King)) {
+                    moves.push(Square.at(location.row, i))
+                    break
+                } else {
+                    break
+                }
             }
             for (let i = location.col + 1; i < 8; i++) {
                 if (board.getPiece(Square.at(location.row, i)) === undefined) {
                     moves.push(Square.at(location.row, i))
-                } else break
+                } else if ((board.getPiece(Square.at(location.row, i))).player === Player.BLACK
+                    && !((board.getPiece(Square.at(location.row, i))) instanceof King)) {
+                    moves.push(Square.at(location.row, i))
+                    break
+                } else {
+                    break
+                }
             }
             for (let i = location.row - 1; i >= 0; i--) {
                 if (board.getPiece(Square.at(i, location.col)) === undefined) {
                     moves.push(Square.at(i, location.col))
-                } else break
+                } else if ((board.getPiece(Square.at(i, location.col))).player === Player.BLACK
+                    && !((board.getPiece(Square.at(i, location.col))) instanceof King)) {
+                    moves.push(Square.at(i, location.col))
+                    break
+                } else {
+                    break
+                }
             }
             for (let i = location.row + 1; i < 8; i++) {
                 if (board.getPiece(Square.at(i, location.col)) === undefined) {
                     moves.push(Square.at(i, location.col))
-                } else break
+                } else if ((board.getPiece(Square.at(i, location.col))).player === Player.BLACK
+                    && !((board.getPiece(Square.at(i, location.col))) instanceof King)) {
+                    moves.push(Square.at(i, location.col))
+                    break
+                } else {
+                    break
+                }
             }
 
             return moves
